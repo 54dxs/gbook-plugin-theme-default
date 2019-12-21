1eb8c0d6226ffc00b6be295cc1be63bd8aa65aa3
深情小建 <374452668@qq.com>
2019-12-21 20:46:12 +0800

    添加

--- src/build.sh
+++ src/build.sh
@@ -1,30 +1,30 @@
 #! /bin/bash
 
-# Cleanup folder
+# 清除文件夹
 rm -rf _assets
 
-# Recreate folder
+# 重新创建文件夹
 mkdir -p _assets/website/
 mkdir -p _assets/ebook/
 
-# Compile JS
+# 编译JS
 browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
 browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
 
-# Compile Website CSS
+# 编译 Website CSS
 lessc -clean-css src/less/website.less _assets/website/style.css
 
-# Compile eBook CSS
+# 编译 eBook CSS
 lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
 lessc -clean-css src/less/pdf.less _assets/ebook/pdf.css
 lessc -clean-css src/less/mobi.less _assets/ebook/mobi.css
 lessc -clean-css src/less/epub.less _assets/ebook/epub.css
 
-# Copy fonts
+# 拷贝 fonts
 mkdir -p _assets/website/fonts
 cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
 
-# Copy icons
+# 拷贝 icons
 mkdir -p _assets/website/images
 cp node_modules/gitbook-logos/output/favicon.ico _assets/website/images/
 cp node_modules/gitbook-logos/output/apple-touch-icon-152.png _assets/website/images/apple-touch-icon-precomposed-152.png
